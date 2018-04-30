34d4bcbeb5289f202a124305607428a198ae87fa
Michael Baron <Michael.Baron@lmc.eu>
2018-04-30 19:02:05 +0200

    Token done

--- app/models/Token.php
+++ app/models/Token.php
@@ -2,12 +2,35 @@
 
 namespace Rss;
 
+use Exception;
+
+/**
+ * Class Token.
+ * Handles request validation and "login".
+ * @package Rss
+ */
 class Token {
+
+	/**
+	 * Validates provided token against correct token.
+	 *
+	 * @param string $token
+	 *
+	 * @throws Exception
+	 */
 	public static function validate( $token ) {
-
+		if(strtolower($token) !== self::createToken()) {
+			throw new Exception('Wrong authentication token!');
+		}
 	}
 
+	/**
+	 * Creates correct token.
+	 *
+	 * @return string
+	 */
 	private static function createToken() {
-		$date = date('YmdH');
+		return strtolower(sha1(APP_LOGIN_SEED . date('YmdH')));
 	}
+
 }
